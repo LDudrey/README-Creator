c2e5210130427f604fc2ac69c07c553720beaef9
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-12-07 23:12:35 -0600

    added questions, template and license info

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 // TODO: Include packages needed for this application
-
+// http://fredkschott.com/post/2014/06/require-and-the-module-system/
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is your GitHub username?',
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'What is your email address?',
+        },
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the project name?',
+        },
+        {
+            name: 'description',
+            type: 'input',
+            message: 'Write a short description about your project.',
+        },
+        {
+            name: 'license',
+            type: 'list',
+            message: 'What license should this project have?',
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        },
+        {
+            name: 'install',
+            type: 'input',
+            message: 'What command should be run to install dependencies?',
+        },
+        {
+            name: 'tests',
+            type: 'input',
+            message: 'What command should be used to run tests?',
+        },
+        {
+            name: 'usage',
+            type: 'input',
+            message: 'What does the user need to know about using this repo?',
+        },
+        {
+            name: 'contribute',
+            type: 'input',
+            message: 'What does the user need to know about contributing to the repo?',
+        },
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile() {
+fs.writeFile('README.md', generate, (err) =>
+err ? console.log(err) : console.log('Success!'))
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const answers = inquirer.prompt(questions);
+}
+return writeToFile();
 
 // Function call to initialize app
-init();
+ init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+// License badges https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      let mit = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "APACHE 2.0":
+      let apache = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GPL 3.0":
+      let gpl = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "BSD 3":
+      let bsd = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "None":
+      let none = " ";
+      break;
+  }
+};
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+    
+  ## License
+  
+  [![License](renderLicenseBadge)](renderLicenseLink)
+  ${license}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.install}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## How to Contribute
+  
+  ${data.contribute}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  Any questions please contact me at: ${data.email}. More examples of my work is available at ${data.name}.`
 }
 
 module.exports = generateMarkdown;
+
